5d1a69836842801ed817cd136e0a939b1e988351
Kolobok <kolobkow.nikita@yandex.ru>
2025-04-13 13:34:17 +0300

    main change

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,75 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+#include <cmath>
+#include <cctype>
+
+unsigned int faStr1(const char* str) {
+    unsigned int count = 0;
+    bool inWord = false;
+    bool containsDigit = false;
+    while (*str) {
+        if (!std::isspace(*str) && !inWord) {
+            count++;
+            inWord = true;
+            containsDigit = false;
+        }
+        if (std::isdigit(*str)) {
+            count -= containsDigit ? 0 : 1;
+            containsDigit = true;
+        }
+        if (std::isspace(*str) && inWord) {
+            containsDigit = false;
+            inWord = false;
+        }
+        str++;
+    }
+    return count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+    unsigned int count = 0;
+    bool inWord = false;
+    bool isValidated = false;
+    while (*str) {
+        if (std::isspace(*str) && inWord) {
+            isValidated = false;
+            inWord = false;
+        }
+        else if (!std::islower(*str)) {
+            count -= isValidated ? 1 : 0;
+            isValidated = false;
+        }
+        else if (!inWord && std::isupper(*str)) {
+            count++;
+            inWord = true;
+            isValidated = true;
+        }
+        str++;
+    }
+    return count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+    unsigned int count = 0;
+    size_t lenWords = 0;
+    bool inWord = false;
+    while (*str) {
+        if (std::isspace(*str) && inWord) {
+            inWord = false;
+        }
+        else if (!std::isspace(*str) && !inWord) {
+            inWord = true;
+            count++;
+            lenWords++;
+        }
+        else if (!std::isspace(*str) && inWord) {
+            lenWords++;
+        }
+        str++;
+    }
+    if (count == 0) {
+        return 0;
+    }
+    return count == 0 ? 0 : std::round(static_cast<double>(lenWords) / count);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 #include "fun.h"
 
 int main() {
-    return 0;
+    std::cout << faStr1("qwe4  gftt e 555gr rrgr  grhh ") << std::endl;
+    std::cout << faStr1("  qwe4 ") << std::endl;
+    std::cout << faStr2("Hello, world!") << std::endl;
+    std::cout << faStr2("Hello world!") << std::endl;
+    std::cout << faStr3(" bad dog ") << std::endl;
+    std::cout << faStr3(" six monkeys") << std::endl;
 }
